7587a12d7299744679726c88b14b8a75a8cef621
mr3m <carlos@prosciana.com>
2016-11-12 22:14:19 +0100

    change memory limit

--- docker/vintalentsolr/config.sh
+++ docker/vintalentsolr/config.sh
@@ -1,9 +1,11 @@
-docker run --name vintalentsolr -d -p 8983:8983 -p 9999:80 -v ~/files/:/var/www/html -m 700M -t shan
+docker run --name vintalentsolr -d -p 8983:8983 -p 9999:80 -v ~/files/:/var/www/html -m 800M --memory-swap -1 -t shan
 docker exec -it --user root vintalentsolr  bash
 apt-get update
 apt-get install apache2 -y
 service apache2 start
 chmod 777 -R /var/www/html
+exit
+docker exec -it vintalentsolr  bash
 cd /opt/solr
 bin/solr create -c files -d example/files/conf
 bin/post -c files /var/www/html
